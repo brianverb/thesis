fe921c33f86941b46276a58f0adabd329c412c77
brian.verbanck <brianverbanck@hotmail.com>
2023-12-05 13:33:19 +0100

    added averages for kabsch and preprocess

--- code/results.py
+++ code/results.py
@@ -6,3 +6,14 @@ results_MTW = np.load("output_MTW.npy")
 
 print("MTMM averages out at: " + str(np.mean(results_MTMM)))
 print("MTW averages out at: " + str(np.mean(results_MTW)))
+
+
+print("MTMM Kabsch Preprocess: " + str(np.mean(results_MTMM[:, :, :, 0, 0])))
+print("MTMM  Preprocess: " + str(np.mean(results_MTMM[:, :, :, 0, 1])))
+print("MTMM Kabsch : " + str(np.mean(results_MTMM[:, :, :, 1, 0])))
+print("MTMM  : " + str(np.mean(results_MTMM[:, :, :, 1, 1])))
+
+print("MTW Kabsch Preprocess: " + str(np.mean(results_MTW[:, :, :, 0, 0])))
+print("MTW  Preprocess: " + str(np.mean(results_MTW[:, :, :, 0, 1])))
+print("MTW Kabsch : " + str(np.mean(results_MTW[:, :, :, 1, 0])))
+print("MTW  : " + str(np.mean(results_MTW[:, :, :, 1, 1])))
